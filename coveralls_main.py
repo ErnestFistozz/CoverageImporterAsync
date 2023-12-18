c4a58a708f5febd49159662af24e237f97382022
Ernest Mamba <ebmamba@shoprite.co.za>
2023-12-18 14:11:31 +0200

    fix: refactoring variable names

--- coveralls_main.py
+++ coveralls_main.py
@@ -4,8 +4,8 @@ from src.coverageimporter import CoverageImporter
 from src.utils import Utils
 
 if __name__ == '__main__':
-    codecov_repositories = Utils.repositories('coveralls_repos.txt')
-    for coveralls_repo in codecov_repositories:
+    coveralls_repositories = Utils.repositories('coveralls_repos.txt')
+    for coveralls_repo in coveralls_repositories:
         coverage_importer = CoverageImporter()
         coveralls = CoverallsCoverage(coveralls_repo[0], coveralls_repo[1])
         coveralls_data = asyncio.get_event_loop().run_until_complete(
