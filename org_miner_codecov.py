ac039062f3a3248e3769eb5299d3c2191636a1de
Ernest Mamba <ebmamba@shoprite.co.za>
2024-01-08 13:24:09 +0200

    adding repositories to be mined via the scripts

---
+++ org_miner_codecov.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from src.utils import Utils
+from src.codecov import CodeCovCoverage
+from src.codecov_logger import CodecovCoverageLogger
+from src.coverageimporter import CodecovCoverageImporter
+from src.codecov_repositories import CodeCovRepositories
+
+orgs = [
+    'yelp', 'cfpb', 'square', 'didi', 'Esri', 'godaddy',
+    'kubernetes', 'cloudflare', 'facebook', 'alibaba',
+]
+# 'ibm', 'apache', 'microsoft', 'adobe',
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    codecov_loger = CodecovCoverageLogger()
+    for org in orgs:
+        repo = CodeCovRepositories(org)
+        repositories = loop.run_until_complete(repo.repositories())
+        for active_repo in repositories:
+            repo_name = active_repo['name']
+            codecov = CodeCovCoverage(org, repo_name, codecov_loger)
+            codecov_importer = CodecovCoverageImporter(codecov)
+            codecov_data = loop.run_until_complete(codecov_importer.analyze_commits())
+            try:
+                Utils.save_into_file('FinalCodecovOrganisationAsyncResults.csv', codecov_data)
+            except Exception as err:
+                print(f'failed to save Codecov data: error - {err}')
+                continue
---
+++ org_miner_coveralls.py
@@ -0,0 +1,34 @@
+import asyncio
+
+from src.utils import Utils
+from src.coveralls import CoverallsCoverage
+from src.coveralls_logger import CoverallsCoverageLogger
+from src.coverageimporter import CoverallsCoverageImporter
+from src.coveralls_repositories import CoverallsRepository
+
+orgs = [
+    'OSGeo', 'Esri', 'cloudflare', 'kubernetes', 'godaddy',
+    'imb', 'square', 'yelp', 'cfpb', 'facebook', 'alibaba',
+]
+# 'apache', 'microsoft', 'adobe',
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    coveralls_loger = CoverallsCoverageLogger()
+    user_agent = {
+        "User-Agent": 'Linux PC/Firefox browser: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 '
+                      'Firefox/15.0.1'}
+    for org in orgs:
+        repo = CoverallsRepository(org, request_headers=user_agent)
+        repositories = loop.run_until_complete(repo.repositories())
+
+        for active_repo in repositories:
+            repo_name = active_repo['name']
+            coveralls = CoverallsCoverage(org, repo_name, coveralls_loger)
+            coveralls_importer = CoverallsCoverageImporter(coveralls)
+            coveralls_data = loop.run_until_complete(coveralls_importer.analyze_commits())
+            try:
+                Utils.save_into_file('FinalCoverallsOrganisationAsyncResults.csv', coveralls_data)
+            except Exception as err:
+                print(f'failed to save Coveralls data: error - {err}')
+                continue
