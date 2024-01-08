843c586e3abfbe62b4f4b07dc2929b5f04924e15
Ernest Mamba <ebmamba@shoprite.co.za>
2024-01-08 15:39:36 +0200

    Undo: undid the change made

--- org_miner_codecov.py
+++ org_miner_codecov.py
@@ -6,11 +6,10 @@ from src.codecov_logger import CodecovCoverageLogger
 from src.coverageimporter import CodecovCoverageImporter
 from src.codecov_repositories import CodeCovRepositories
 
-orgs = ['ErnestFistozz']
-# [
-#     'yelp', 'cfpb', 'square', 'didi', 'Esri', 'godaddy',
-#     'kubernetes', 'cloudflare', 'facebook', 'alibaba',
-# ]
+orgs =  [
+    'yelp', 'cfpb', 'square', 'didi', 'Esri', 'godaddy',
+    'kubernetes', 'cloudflare', 'facebook', 'alibaba',
+]
 # 'ibm', 'apache', 'microsoft', 'adobe',
 
 if __name__ == '__main__':
@@ -20,13 +19,12 @@ if __name__ == '__main__':
         repo = CodeCovRepositories(org)
         repositories = loop.run_until_complete(repo.repositories())
         for active_repo in repositories:
-            print(active_repo['name'])
-            # repo_name = active_repo['name']
-            # codecov = CodeCovCoverage(org, repo_name, codecov_loger)
-            # codecov_importer = CodecovCoverageImporter(codecov)
-            # codecov_data = loop.run_until_complete(codecov_importer.analyze_commits())
-            # try:
-            #     Utils.save_into_file('FinalCodecovOrganisationAsyncResults.csv', codecov_data)
-            # except Exception as err:
-            #     print(f'failed to save Codecov data: error - {err}')
-            #     continue
+            repo_name = active_repo['name']
+            codecov = CodeCovCoverage(org, repo_name, codecov_loger)
+            codecov_importer = CodecovCoverageImporter(codecov)
+            codecov_data = loop.run_until_complete(codecov_importer.analyze_commits())
+            try:
+                Utils.save_into_file('FinalCodecovOrganisationAsyncResults.csv', codecov_data)
+            except Exception as err:
+                print(f'failed to save Codecov data: error - {err}')
+                continue
